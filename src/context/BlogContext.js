437d5ee6cb60defbab83bbee0309a2124e115386
adiwawan90 <adi.wawan90@gmail.com>
2020-04-11 23:01:38 +0700

    nambah logic lain

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,25 +1,58 @@
 import createDataContext from './createDataContext';
-import React, {useReducer} from 'react';
+// import React, {useReducer} from 'react';
 
 // menambahkan use reducer
 // 1. buat func reducer
 const blogReducer = (state, action) => {
     switch(action.type){
+        case 'edit_blogPost':
+            return  state.map((blogPost) => {
+                return blogPost.id === action.payload.id ? action.payload : blogPost
+            })
         case 'add_blogpost':
-            return [...state, {title: `Blog Post #${state.length}`}];
+            return [
+                ...state, {
+                    id: Math.floor(Math.random() * 99999), 
+                    title: action.payload.title,
+                    content: action.payload.content
+                    }
+            ];
+        case 'delete_blogpost':
+            return state.filter((blogPost) => blogPost.id !== action.payload);
         default:
             return state;
     }
 };
 
 const addBlogPost = (dispatch) => {
-    return () => {
-        dispatch({ type: 'add_blogpost'});
+    return (title, content, callback) => {
+        dispatch({ type: 'add_blogpost', payload: {title, content}});
+        if (callback) {
+            callback();
+        }
+    };
+};
+
+const deleteBlogPost = (dispatch) => {
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id })
+    };
+};
+
+const editBlogPost= (dispatch) => {
+    return (id, title, content, callback) => {
+        dispatch({
+            type: 'edit_blogPost',
+            payload: { id: id, title: title, content: content}
+        });
+        if (callback) {
+            callback();
+        }
     };
 };
 
 export const { Context, Provider } = createDataContext(
     blogReducer, 
-    {addBlogPost}, 
-    []
+    {addBlogPost, deleteBlogPost, editBlogPost}, 
+    [{ title: 'TEST', content: 'SAY', id: 1}]
 );
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,26 +1,63 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import { Context } from '../context/BlogContext';
 // import BlogContext from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
-    const { state, addBlogPost} = useContext(Context);
+const IndexScreen = ({ navigation }) => {
+    const { state, addBlogPost, deleteBlogPost} = useContext(Context);
     return (
         <View>
             <Text>Ini Index Sceen ya</Text>
-            <Button title="Add Post" onPress={addBlogPost} />
             <FlatList
                 data={state}
                 keyExtractor={(blogPost) => blogPost.title}
                 renderItem={({ item }) => {
-                    return <Text>{ item.title}</Text>
+                    return (
+                        <TouchableOpacity onPress={() => navigation.navigate('Show', {id: item.id, title: item.title})}>
+                            <View style={styles.row}>
+                                <Text style={styles.title} >{ item.title} - {item.id}</Text>
+                                <TouchableOpacity onPress={() => deleteBlogPost(item.id)} >
+                                    <Feather name="trash" 
+                                        style={styles.icon}
+                                    />
+                                </TouchableOpacity>
+                            </View>
+                        </TouchableOpacity>
+                    );       
                 }}
             />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+// menambah icon &  functional i header
+IndexScreen.navigationOptions = ({navigation}) => {
+    return {
+        headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+                <Feather name="plus" size={30} />
+            </TouchableOpacity>
+        )
+    };
+};
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal:10,
+        borderTopWidth: 1,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24,
+    }
+});
 
 export default IndexScreen;
